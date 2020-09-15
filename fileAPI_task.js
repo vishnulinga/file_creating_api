86c50afb7ed8b5ea5b133fa2ae1e38276bbe745d
vishnulinga <67555130+vishnulinga@users.noreply.github.com>
2020-09-15 15:44:09 +0530

    Add files via upload

--- fileAPI_task.js
+++ fileAPI_task.js
@@ -6,14 +6,25 @@ var time=date.toLocaleTimeString()
 time=time.replace(/:/g, " ");
 var filename=date.toDateString()+"-"+time;
 console.log(filename)
-
+var fileslist="Current directory filenames:<br>"
 fs.writeFile(`${filename}.txt`, `${txt}`, function (err) {
     if (err) throw err;
     console.log('Saved!');
+    fs.readdir(".", (err, files) => { 
+      if (err) 
+        console.log(err); 
+      else { 
+        console.log("Current directory filenames:<br>"); 
+        files.forEach(file => { 
+          fileslist+=file+"<br>"
+        })
+      }
+    }); 
+
   });
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(`File "${filename}.txt" got created`);
+  res.write(`File "${filename}.txt" got created`+`<br>${fileslist}`);
     res.end();
   }).listen(process.env.PORT || 8080);
